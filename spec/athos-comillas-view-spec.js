7ba61f3dc5c123d2783883dfd20ff600c28534d9
Athos Orio <athos.oc@gmail.com>
2019-01-01 19:46:34 +0100

    First commit

---
+++ spec/athos-comillas-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AthosComillasView from '../lib/athos-comillas-view';
+
+describe('AthosComillasView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
